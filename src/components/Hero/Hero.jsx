6412b40b83168b383f95fdcd2e07b9814db921d3
DrDawg333 <ashmaan2580@gmail.com>
2024-11-22 14:48:10 +0530

    updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,81 +1,98 @@
-import React from 'react'
-import './Hero.css'
-import Header from '../Header/Header'
+import React from "react";
+import "./Hero.css";
+import Header from "../Header/Header";
 import hero_image from "../../assets/hero_image.png";
 import hero_image_back from "../../assets/hero_image_back.png";
 import Heart from "../../assets/heart.png";
 import Calories from "../../assets/calories.png";
 
+import { motion } from "framer-motion";
 
 const Hero = () => {
+  const transition = { tpye:'string', duration: 2, ease: "easeInOut" };
+
   return (
     <div className="hero">
-        <div className="blur blur-h"></div>
-        <div className="left-h">
-            <Header/>
-    {/* the best ad  */}
-            <div className="the-best-ad">
-              <div></div>
-              <span>The best fitness club in da hood</span>
-            </div>
-    {/* hero heading  */}
-            <div className="hero-text">
-              <div>
-                <span className='stroke-text'>Shape</span>
-                <span> Your</span>
-              </div>
-              <div>
-                <span>Ideal Body</span>
-              </div>
-              <div>
-                <span>
-                In here we make your dream come true
-                </span>
-              </div>
-            </div>
-    {/* figures */}
-            <div className="figures">
-              <div>
-                <span>+100</span>
-                <span>expert coaches</span>
-              </div>
-              <div>
-                <span>+1000</span>
-                <span>members</span>
-              </div>
-              <div>
-                <span>+50</span>
-                <span>fitness programs</span>
-              </div>
-            </div>
-      {/* hero buttons */}
-            <div className="hero-buttons">
-              <button className="btn">Get Started</button>
-              <button className="btn">Learn More</button>
-            </div>
+      <div className="blur blur-h"></div>
+      <div className="left-h">
+        <Header />
+        {/* the best ad  */}
+        <div className="the-best-ad">
+          <motion.div
+            animate={{ left: "8px" }}
+            initial={{ left: "238px" }}
+            transition={transition}
+          ></motion.div>
+          <span>The best fitness club in da hood</span>
         </div>
-        <div className="right-h">
-          <button className='btn'>Join Now</button>
+        {/* hero heading  */}
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text">Shape</span>
+            <span> Your</span>
+          </div>
+          <div>
+            <span>Ideal Body</span>
+          </div>
+          <div>
+            <span>In here we make your dream come true</span>
+          </div>
+        </div>
+        {/* figures */}
+        <div className="figures">
+          <div>
+            <span>+100</span>
+            <span>expert coaches</span>
+          </div>
+          <div>
+            <span>+1000</span>
+            <span>members</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+        {/* hero buttons */}
+        <div className="hero-buttons">
+          <button className="btn">Get Started</button>
+          <button className="btn">Learn More</button>
+        </div>
+      </div>
+      <div className="right-h">
+        <button className="btn">Join Now</button>
 
-          <div className="heart-rate">
-            <img src={Heart} alt="" />
-            <span>Heart Rate</span>
-            <span>120 bpm</span>
+        <motion.div 
+         initial={{right:"-1rem"}}
+         whileInView={{right:"4rem"}}
+         transition={{ duration: 1, ease: "easeInOut" }}
+         className="heart-rate">
+          <img src={Heart} alt="" />
+          <span>Heart Rate</span>
+          <span>120 bpm</span>
+        </motion.div>
+        {/* hero images  */}
+        <img src={hero_image} alt="" className="hero-image" />
+        <motion.img
+          initial={{right: "11rem"}}
+          whileInView={{right: "32rem"}}
+          transition={transition}
+          src={hero_image_back} alt="" className="hero-image-back" />
+        {/* claories  */}
+        <motion.div 
+         initial={{right: "50rem"}}
+         whileInView={{right: "38rem"}}
+         transition={transition}
+         className="calories">
+          <img src={Calories} alt="" />
+          <div>
+            <span>Calories Burned</span>
+            <span>220 kcal</span>
           </div>
-      {/* hero images  */}
-          <img src={hero_image} alt="" className='hero-image'/>
-          <img src={hero_image_back} alt="" className='hero-image-back'/>
-      {/* claories  */}
-          <div className='calories'>
-            <img src={Calories} alt=""/>
-            <div>
-              <span>Calories Burned</span>
-              <span>220 kcal</span>
-            </div>
-          </div>
-        </div>
+        </motion.div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -1,25 +1,32 @@
-import image1 from "../assets/t-image1.png";
-import image2 from "../assets/t-image2.jpg";
-import image3 from "../assets/UV.png";
+import image1 from "../assets/Samit.jpg";
+import image2 from "../assets/Yadav.jpg";
+import image3 from "../assets/UV.jpg";
+import image4 from "../assets/Ashmit.jpg";
 
 export const testimonialsData = [
   {
     image: image1,
     review:
       "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
-    name: 'MATHEW HENDRICKSON',
+    name: 'Samit Ranta',
     status : 'ENTREPRENEUR'
   },
   {
     image: image2,
     review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam, ab itaque nam perferendis impedit sint ',
-    name: 'JOHN KEVIN',
+    name: 'Nilaksh Yadav',
     status: 'COACH'
   },
   {
     image : image3,
-    review:' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima aspernatur quod voluptatem',
+    review:' Babu zii mauzza aa gya ye zim mai aake. Moi to bol raha hu ki tum jhona bhi join karlo moost h inka systummm. ',
     name: 'YUVRAJ',
     status: "CUSTOMER"
-  }
+  },
+  {
+    image : image4,
+    review:' Babu zii mauzza aa gya ye zim mai aake. Moi to bol raha hu ki tum jhona bhi join karlo moost h inka systummm. ',
+    name: 'Ashmit Thakur',
+    status: "GYM Owner"
+  },
 ];
